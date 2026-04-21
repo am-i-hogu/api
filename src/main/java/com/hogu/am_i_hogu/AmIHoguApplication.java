88d367c11e0b9f005eae32ecbcb844712d7fee79
mindolii <minjo2003@naver.com>
2026-04-21 11:22:30 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/hogu/am_i_hogu/AmIHoguApplication.java
@@ -0,0 +1,13 @@
+package com.hogu.am_i_hogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmIHoguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmIHoguApplication.class, args);
+	}
+
+}
