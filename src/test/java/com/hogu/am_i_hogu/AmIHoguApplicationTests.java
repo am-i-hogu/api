fe6792525b8e63f897b655540401523811201173
mindolii <minjo2003@naver.com>
2026-04-21 11:22:30 +0900

    feat: 프로젝트 세팅

---
+++ src/test/java/com/hogu/am_i_hogu/AmIHoguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogu.am_i_hogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmIHoguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
