a513c84096eec8324af89b41a744c6cadf8d79ea
mindolii <minjo2003@naver.com>
2026-04-22 22:39:58 +0900

    feat: H2 테스트 환경 구성

--- src/test/java/com/hogu/am_i_hogu/AmIHoguApplicationTests.java
+++ src/test/java/com/hogu/am_i_hogu/AmIHoguApplicationTests.java
@@ -2,8 +2,10 @@ package com.hogu.am_i_hogu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AmIHoguApplicationTests {
 
 	@Test
