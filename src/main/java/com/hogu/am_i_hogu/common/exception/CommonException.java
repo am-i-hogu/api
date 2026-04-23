609516259f693564e847632b87308fafc7e928e8
mindolii <minjo2003@naver.com>
2026-04-23 22:28:47 +0900

    comment: 500 응답 관련 주석 추가 #5

--- src/main/java/com/hogu/am_i_hogu/common/exception/CommonException.java
+++ src/main/java/com/hogu/am_i_hogu/common/exception/CommonException.java
@@ -9,12 +9,14 @@ public class CommonException extends RuntimeException {
     private final CommonErrorCode code;
     private final List<ErrorResponse.ErrorDetail> errors;
 
+    // 상세 오류 없이 throw 할 때 사용
     public CommonException(CommonErrorCode code) {
         super(code.getCode());
         this.code = code;
         this.errors = null;
     }
 
+    // 상세 오류 리스트와 함께 throw 할 때 사용
     public CommonException(CommonErrorCode code, List<ErrorResponse.ErrorDetail> errors) {
         super(code.getCode());
         this.code = code;
