26814a9497fdda48788dee6b6c2b4cdfe7b9e92a
mindolii <minjo2003@naver.com>
2026-04-29 23:36:05 +0900

    chore: ID 생성 전략 TSID로 교체 #11

--- src/test/java/com/hogu/am_i_hogu/domain/oauth/OAuthServiceTest.java
+++ src/test/java/com/hogu/am_i_hogu/domain/oauth/OAuthServiceTest.java
@@ -1,5 +1,6 @@
 package com.hogu.am_i_hogu.domain.oauth;
 
+import com.hogu.am_i_hogu.common.util.TsidGenerator;
 import com.hogu.am_i_hogu.domain.oauth.config.GoogleOAuthProperties;
 import com.hogu.am_i_hogu.domain.oauth.domain.OAuthLoginState;
 import com.hogu.am_i_hogu.domain.oauth.domain.OAuthProvider;
@@ -19,8 +20,9 @@ public class OAuthServiceTest {
 
     private final GoogleOAuthProperties googleOAuthProperties = mock(GoogleOAuthProperties.class);
     private final OAuthLoginStateRepository oauthLoginStateRepository = mock(OAuthLoginStateRepository.class);
+    private final TsidGenerator tsidGenerator = mock(TsidGenerator.class);
     private final OAuthService oauthService =
-            new OAuthService(googleOAuthProperties, oauthLoginStateRepository);
+            new OAuthService(googleOAuthProperties, oauthLoginStateRepository, tsidGenerator);
 
     @Test
     void getAuthorizationUrlTest() {
@@ -32,11 +34,14 @@ public class OAuthServiceTest {
                 .thenReturn("http://localhost:8080/api/auth/callback/GOOGLE");
         when(googleOAuthProperties.getScope())
                 .thenReturn("openid");
+        when(tsidGenerator.nextId())
+                .thenReturn(1L);
 
         String authorizationUrl = oauthService.getAuthorizationUrl(OAuthProvider.GOOGLE);
 
         ArgumentCaptor<OAuthLoginState> captor = ArgumentCaptor.forClass(OAuthLoginState.class);
         verify(oauthLoginStateRepository).save(captor.capture());
+        verify(tsidGenerator).nextId();
         OAuthLoginState savedState = captor.getValue();
 
         Map<String, List<String>> params = UriComponentsBuilder
@@ -44,6 +49,7 @@ public class OAuthServiceTest {
                 .build()
                 .getQueryParams();
 
+        assertThat(savedState.getId()).isEqualTo(1L);
         assertThat(authorizationUrl).isNotBlank();
         assertThat(params.get("client_id").get(0)).isEqualTo("test-client-id");
         assertThat(params.get("response_type").get(0)).isEqualTo("code");
