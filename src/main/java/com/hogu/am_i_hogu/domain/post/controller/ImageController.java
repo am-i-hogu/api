c3d563ac8c96d04cb762088f21f3cd6741c04402
mj0107 <alswns1285@gmail.com>
2026-05-01 00:48:48 +0900

    refactor: 생성자 lombok의 @RequiredArgsConstructor 사용하도록 수정 #6

--- src/main/java/com/hogu/am_i_hogu/domain/post/controller/ImageController.java
+++ src/main/java/com/hogu/am_i_hogu/domain/post/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.hogu.am_i_hogu.domain.post.controller;
 
 import com.hogu.am_i_hogu.domain.post.dto.response.ImageUploadResponse;
 import com.hogu.am_i_hogu.domain.post.service.ImageUploadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +13,11 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/images")
 public class ImageController {
 
     private final ImageUploadService imageUploadService;
-
-    public ImageController(ImageUploadService imageUploadService) {
-        this.imageUploadService = imageUploadService;
-    }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ImageUploadResponse> uploadImage(
