0538482f9ce4b29afb79a740eb1190080e171ba2
mindolii <minjo2003@naver.com>
2026-04-30 12:59:15 +0900

    feat: DB 조회 및 상태 값 검증 로직 구현 #13

--- src/main/java/com/hogu/am_i_hogu/domain/oauth/repository/OAuthLoginStateRepository.java
+++ src/main/java/com/hogu/am_i_hogu/domain/oauth/repository/OAuthLoginStateRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface OAuthLoginStateRepository extends JpaRepository<OAuthLoginState, Long> {
+    OAuthLoginState findByState(Long state);
 }
