494f6ce973d1dd23382792d9c567e707ad758d59
mindolii <minjo2003@naver.com>
2026-04-30 13:30:55 +0900

    feat: callback 처리용 설정 추가 및 state 검증 로직 구현  #13

--- src/main/java/com/hogu/am_i_hogu/domain/oauth/config/GoogleOAuthProperties.java
+++ src/main/java/com/hogu/am_i_hogu/domain/oauth/config/GoogleOAuthProperties.java
@@ -15,5 +15,6 @@ public class GoogleOAuthProperties {
     private String clientSecret;        // google 측에서 발급받은 client secret
     private String redirectUri;         // google 로그인 인증 완료 후 인가 코드를 전달받을 callback URL
     private String authorizationUri;    // google 로그인 페이지로 이동시키기 위한 google 인증 서버의 endpoint URL
+    private String tokenUri;            // token 교환 endpoint
     private String scope;               // 유저에게 권한을 요구할 정보의 범위
 }
