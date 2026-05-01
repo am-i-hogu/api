128c5b793c3be6aa25f1c793b11a1a4a77136653
mj0107 <alswns1285@gmail.com>
2026-05-01 16:55:51 +0900

    feat: Post 상세 조회용 이미지 조회 메서드 추가 #6

--- src/main/java/com/hogu/am_i_hogu/domain/post/repository/ImageAssetRepository.java
+++ src/main/java/com/hogu/am_i_hogu/domain/post/repository/ImageAssetRepository.java
@@ -4,6 +4,9 @@ import com.hogu.am_i_hogu.domain.post.domain.ImageAsset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageAssetRepository extends JpaRepository<ImageAsset, Long> {
+    List<ImageAsset> findByPost_IdOrderBySortOrderAsc(Long postId);
 }
