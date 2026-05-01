57cdbdd8d786a80c2fa612a906de6b4cf96ac26b
mindolii <minjo2003@naver.com>
2026-05-01 14:05:59 +0900

    feat: onboarding에 쓰일 DTO, Repository 구현 #15

--- src/main/java/com/hogu/am_i_hogu/domain/oauth/repository/RegisterSessionRepository.java
+++ src/main/java/com/hogu/am_i_hogu/domain/oauth/repository/RegisterSessionRepository.java
@@ -3,8 +3,11 @@ package com.hogu.am_i_hogu.domain.oauth.repository;
 import com.hogu.am_i_hogu.domain.oauth.domain.RegisterSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * 회원가입 세션의 영속성 관리
  */
 public interface RegisterSessionRepository extends JpaRepository<RegisterSession, Long> {
+    Optional<RegisterSession> findBySocialAccountId(Long socialAccountId);
 }
