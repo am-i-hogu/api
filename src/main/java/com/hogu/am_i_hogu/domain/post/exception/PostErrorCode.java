4121af9b2b592dc132578b0a346e8ef8e05bbcb4
mj0107 <alswns1285@gmail.com>
2026-05-01 14:32:34 +0900

    feat: Post 상세 조회 에러 코드 구현 #6

--- src/main/java/com/hogu/am_i_hogu/domain/post/exception/PostErrorCode.java
+++ src/main/java/com/hogu/am_i_hogu/domain/post/exception/PostErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum PostErrorCode implements ErrorCodeType {
     EMPTY_REQUEST_BODY(HttpStatus.BAD_REQUEST, "EMPTY_REQUEST_BODY"),
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "INVALID_INPUT_VALUE");
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "INVALID_INPUT_VALUE"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "POST_NOT_FOUND"),
+    POST_ALREADY_DELETED(HttpStatus.NOT_FOUND, "POST_ALREADY_DELETED");
 
     private final HttpStatus status;
     private final String code;
