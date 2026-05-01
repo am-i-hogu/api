f17eacdf1d30e371a73f89c4c7cf322ecb0a5c8d
mj0107 <alswns1285@gmail.com>
2026-05-01 16:49:04 +0900

    feat: Post 상세 조회 Controller 구현 #6

--- src/main/java/com/hogu/am_i_hogu/domain/post/controller/PostController.java
+++ src/main/java/com/hogu/am_i_hogu/domain/post/controller/PostController.java
@@ -2,22 +2,29 @@ package com.hogu.am_i_hogu.domain.post.controller;
 
 import com.hogu.am_i_hogu.domain.post.dto.request.PostCreateRequest;
 import com.hogu.am_i_hogu.domain.post.dto.response.PostCreateResponse;
+import com.hogu.am_i_hogu.domain.post.dto.response.PostDetailResponse;
 import com.hogu.am_i_hogu.domain.post.service.PostCreateService;
+import com.hogu.am_i_hogu.domain.post.service.PostDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController {
-
     private final PostCreateService postCreateService;
+    private final PostDetailService postDetailService;
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostDetailResponse> getPostById(@PathVariable Long postId, Authentication authentication) {
+        Long viewerUserId = authentication == null ? null : Long.valueOf(authentication.getName());
+        PostDetailResponse response = postDetailService.getDetail(postId, viewerUserId);
+
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping
     public ResponseEntity<PostCreateResponse> createPost(
