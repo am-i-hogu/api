59676daa23f6b35bb6f349044e73401c93fd026f
mindolii <minjo2003@naver.com>
2026-05-03 22:48:45 +0900

    refactor: Entity, Repository 도메인 정리 #15

--- src/main/java/com/hogu/am_i_hogu/domain/User/repository/UserRepository.java
+++ src/main/java/com/hogu/am_i_hogu/domain/User/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.hogu.am_i_hogu.domain.auth.repository;
+package com.hogu.am_i_hogu.domain.User.repository;
 
-import com.hogu.am_i_hogu.domain.auth.domain.User;
+import com.hogu.am_i_hogu.domain.User.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
