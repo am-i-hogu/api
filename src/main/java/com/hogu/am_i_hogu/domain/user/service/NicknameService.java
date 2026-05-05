b77f91e727ffa88338fb146a614ebc430998b019
mindolii <minjo2003@naver.com>
2026-05-06 01:09:17 +0900

    feat: 닉네임 중복 확인 기본 구조 구현 #19
    # Conflicts:
    #	src/main/java/com/hogu/am_i_hogu/domain/user/domain/User.java
    #	src/main/java/com/hogu/am_i_hogu/domain/user/domain/UserHoguStat.java
    #	src/main/java/com/hogu/am_i_hogu/domain/user/repository/UserHoguStatRepository.java
    #	src/main/java/com/hogu/am_i_hogu/domain/user/repository/UserRepository.java

---
+++ src/main/java/com/hogu/am_i_hogu/domain/user/service/NicknameService.java
@@ -0,0 +1,60 @@
+package com.hogu.am_i_hogu.domain.user.service;
+
+import com.hogu.am_i_hogu.common.exception.CustomException;
+import com.hogu.am_i_hogu.common.exception.ErrorResponse;
+import com.hogu.am_i_hogu.domain.user.dto.response.CheckNicknameResponse;
+import com.hogu.am_i_hogu.domain.user.exception.UserErrorCode;
+import com.hogu.am_i_hogu.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class NicknameService {
+    private final UserRepository userRepository;
+
+    public NicknameService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public CheckNicknameResponse checkNickname(String nickname) {
+        validateNickname(nickname);
+        boolean isAvailable = isNicknameAvailable(nickname);
+
+        return new CheckNicknameResponse(isAvailable);
+    }
+
+    private void validateNickname(String nickname) {
+        List<ErrorResponse.ErrorDetail> errors = new ArrayList<>();
+
+        if (nickname == null || nickname.isBlank()) {               // 닉네임이 비어있는 경우
+            errors.add(new ErrorResponse.ErrorDetail(
+                    "nickname",
+                    "EMPTY_NICKNAME"
+            ));
+            throw new CustomException(UserErrorCode.INVALID_INPUT_VALUE, errors);
+        }
+
+        if (!nickname.matches("^[가-힣a-zA-Z0-9]+$")) {        // 닉네임에 특수문자가 포함된 경우
+            errors.add(new ErrorResponse.ErrorDetail(
+                    "nickname",
+                    "SPECIAL_CHAR_NICKNAME"
+            ));
+        }
+        if (nickname.length() < 2 || nickname.length() > 20) {      // 닉네임 길이 위반한 경우
+            errors.add(new ErrorResponse.ErrorDetail(
+                    "nickname",
+                    "NICKNAME_LENGTH_EXCEEDED"
+            ));
+        }
+
+        if (!errors.isEmpty()) {
+            throw new CustomException(UserErrorCode.INVALID_INPUT_VALUE, errors);
+        }
+    }
+
+    private boolean isNicknameAvailable(String nickname) {
+        return !userRepository.existsByNickname(nickname);
+    }
+}
