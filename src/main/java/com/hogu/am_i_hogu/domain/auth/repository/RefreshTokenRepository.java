3255b01d9fb608fe0081270fccae85a673a24134
mindolii <minjo2003@naver.com>
2026-05-05 22:17:22 +0900

    feat: refresh token 조회 메서드에 lock 기능 구현 #17

--- src/main/java/com/hogu/am_i_hogu/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/hogu/am_i_hogu/domain/auth/repository/RefreshTokenRepository.java
@@ -1,7 +1,9 @@
 package com.hogu.am_i_hogu.domain.auth.repository;
 
 import com.hogu.am_i_hogu.domain.auth.domain.RefreshToken;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
@@ -9,5 +11,6 @@ import java.util.Optional;
  * refresh token의 영속성 관리
  */
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<RefreshToken> findById(Long id);
 }
