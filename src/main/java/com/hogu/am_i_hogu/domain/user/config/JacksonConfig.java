9c8fb8b8a08cc3d20919d9c69399e9201a2b2f37
mindolii <minjo2003@naver.com>
2026-05-07 00:33:30 +0900

    feat: 사용자 프로필 수정 요청 검증 구현 #19

---
+++ src/main/java/com/hogu/am_i_hogu/domain/user/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+package com.hogu.am_i_hogu.domain.user.config;
+
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        Jackson2ObjectMapperBuilder builder =
+                new Jackson2ObjectMapperBuilder();
+        builder.modulesToInstall(new JsonNullableModule());
+
+        return builder;
+    }
+}
--- src/main/java/com/hogu/am_i_hogu/domain/user/controller/UserController.java
+++ src/main/java/com/hogu/am_i_hogu/domain/user/controller/UserController.java
@@ -1,26 +1,45 @@
 package com.hogu.am_i_hogu.domain.user.controller;
 
+import com.hogu.am_i_hogu.domain.user.dto.request.UpdateProfileRequest;
 import com.hogu.am_i_hogu.domain.user.dto.response.CheckNicknameResponse;
+import com.hogu.am_i_hogu.domain.user.dto.response.UpdateProfileResponse;
 import com.hogu.am_i_hogu.domain.user.service.NicknameService;
+import com.hogu.am_i_hogu.domain.user.service.UpdateProfileService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
     private final NicknameService nicknameService;
+    private final UpdateProfileService updateProfileService;
 
-    public UserController(NicknameService nicknameService) {
+    public UserController(
+            NicknameService nicknameService,
+            UpdateProfileService updateProfileService
+    ) {
         this.nicknameService = nicknameService;
+        this.updateProfileService = updateProfileService;
     }
 
-    @GetMapping("/api/users/check-nickname")
+    @PatchMapping("/me")
+    public ResponseEntity<UpdateProfileResponse> updateProfile(
+            Authentication authentication,
+            @RequestBody(required = false) UpdateProfileRequest request
+    ) {
+        Long userId = Long.valueOf(authentication.getName());
+        UpdateProfileResponse response = updateProfileService.updateProfile(userId, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/check-nickname")
     public ResponseEntity<CheckNicknameResponse> checkNickname(
             @RequestParam(name="nickname") String nickname
     ) {
         CheckNicknameResponse response = nicknameService.checkNickname(nickname);
-        return ResponseEntity.status(200)
-                .body(response);
+        return ResponseEntity.ok(response);
     }
 }
