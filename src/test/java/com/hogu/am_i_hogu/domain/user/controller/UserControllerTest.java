5d261efe8c2d4c855af55e955f8e182e80b48407
mindolii <minjo2003@naver.com>
2026-05-07 12:40:32 +0900

    test: 닉네임 중복 검사 실패 테스트 추가 #19

--- src/test/java/com/hogu/am_i_hogu/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/hogu/am_i_hogu/domain/user/controller/UserControllerTest.java
@@ -96,4 +96,73 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.isAvailable").value(false));
     }
+
+    /**
+     * 닉네임 중복 검사 실패 테스트:
+     * 비어있는 nickname을 요청으로 보내고,
+     * - (1) 응답 status가 404인지 확인
+     * - (2) <필드 정보: nickname, 오류 코드: EMPTY_NICKNAME> 반환 확인
+     */
+    @Test
+    void nicknameCheckRejectsEmptyNickname() throws Exception {
+        mockMvc.perform(get("/api/users/check-nickname")
+                    .param("nickname", " "))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("INVALID_INPUT_VALUE"))
+                .andExpect(jsonPath("$.errors[0].field").value("nickname"))
+                .andExpect(jsonPath("$.errors[0].code").value("EMPTY_NICKNAME"));
+    }
+
+    /**
+     * 닉네임 중복 검사 실패 테스트:
+     * 특수문자를 포함한 nickname을 요청으로 보내고,
+     * - (1) 응답 status가 404인지 확인
+     * - (2) <필드 정보: nickname, 오류 코드: SPECIAL_CHAR_NICKNAME> 반환 확인
+     */
+    @Test
+    void nicknameCheckRejectsSpecialChar() throws Exception {
+        mockMvc.perform(get("/api/users/check-nickname")
+                    .param("nickname", "special char nickname"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("INVALID_INPUT_VALUE"))
+                .andExpect(jsonPath("$.errors[0].field").value("nickname"))
+                .andExpect(jsonPath("$.errors[0].code").value("SPECIAL_CHAR_NICKNAME"));
+    }
+
+    /**
+     * 닉네임 중복 검사 실패 테스트:
+     * 길이가 20을 초과하는 nickname을 요청으로 보내고,
+     * - (1) 응답 status가 404인지 확인
+     * - (2) <필드 정보: nickname, 오류 코드: NICKNAME_LENGTH_EXCEEDED> 반환 확인
+     */
+    @Test
+    void nicknameCheckRejectsLongNickname() throws Exception {
+        String nickname = "thisisverylongnickname";
+        mockMvc.perform(get("/api/users/check-nickname")
+                    .param("nickname", nickname))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("INVALID_INPUT_VALUE"))
+                .andExpect(jsonPath("$.errors[0].field").value("nickname"))
+                .andExpect(jsonPath("$.errors[0].code").value("NICKNAME_LENGTH_EXCEEDED"));
+    }
+
+    /**
+     * 닉네임 중복 검사 실패 테스트:
+     * 특수문자를 포함하며 길이가 20을 초과하는 nickname을 요청으로 보내고,
+     * - (1) 응답 status가 404인지 확인
+     * - (2) <필드 정보: nickname, 오류 코드: SPECIAL_CHAR_NICKNAME>,
+     *      <필드 정보: nickname, 오류 코드: NICKNAME_LENGTH_EXCEEDED 반환 확인
+     */
+    @Test
+    void nicknameCheckRejectsSpecialCharAndLongNickname() throws Exception {
+        String nickname = "this is very long nickname";
+        mockMvc.perform(get("/api/users/check-nickname")
+                .param("nickname", nickname))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("INVALID_INPUT_VALUE"))
+                .andExpect(jsonPath("$.errors[0].field").value("nickname"))
+                .andExpect(jsonPath("$.errors[0].code").value("SPECIAL_CHAR_NICKNAME"))
+                .andExpect(jsonPath("$.errors[1].field").value("nickname"))
+                .andExpect(jsonPath("$.errors[1].code").value("NICKNAME_LENGTH_EXCEEDED"));
+    }
 }
