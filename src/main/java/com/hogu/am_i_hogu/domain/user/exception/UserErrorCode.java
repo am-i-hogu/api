a22ee0a8ed9251fc808dbf598d19a00cb765e9f1
mindolii <minjo2003@naver.com>
2026-05-09 22:53:17 +0900

    style: 닉네임 중복 체크 오류 코드 수정 #19

--- src/main/java/com/hogu/am_i_hogu/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/hogu/am_i_hogu/domain/user/exception/UserErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorCode implements ErrorCodeType {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "INVALID_INPUT_VALUE"),
+    INVALID_PARAM_VALUE(HttpStatus.BAD_REQUEST, "INVALID_PARAM_VALUE"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND"),
     EMPTY_REQUEST_BODY(HttpStatus.BAD_REQUEST, "EMPTY_REQUEST_BODY"),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "DUPLICATE_NICKNAME");
