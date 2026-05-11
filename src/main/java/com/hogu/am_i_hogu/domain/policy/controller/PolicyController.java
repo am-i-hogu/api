30e1e1d3e7c5b194bb532773e55b7d7bfda25572
mindolii <minjo2003@naver.com>
2026-05-11 16:33:48 +0900

    feat: 개인정보 처리 방침 controller 구현 #29

--- src/main/java/com/hogu/am_i_hogu/domain/policy/controller/PolicyController.java
+++ src/main/java/com/hogu/am_i_hogu/domain/policy/controller/PolicyController.java
@@ -1,5 +1,22 @@
 package com.hogu.am_i_hogu.domain.policy.controller;
 
+import com.hogu.am_i_hogu.domain.policy.dto.response.PolicyResponse;
+import com.hogu.am_i_hogu.domain.policy.service.PrivacyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PolicyController {
 
+    private final PrivacyService privacyService;
+
+    public PolicyController(PrivacyService privacyService) {
+        this.privacyService = privacyService;
+    }
+
+    @GetMapping("/api/policies/privacy")
+    public ResponseEntity<PolicyResponse> getPrivacyPolicy() {
+        return ResponseEntity.ok(privacyService.getPrivacyPolicy());
+    }
 }
