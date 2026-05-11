4d067f9ceeffb44c1d47b6c00f6f1b75e17a9c05
mindolii <minjo2003@naver.com>
2026-05-11 15:50:21 +0900

    feat: policy revision Entity 구현 #29

---
+++ src/main/java/com/hogu/am_i_hogu/domain/policy/service/PrivacyService.java
@@ -0,0 +1,13 @@
+package com.hogu.am_i_hogu.domain.policy.service;
+
+import com.hogu.am_i_hogu.domain.policy.dto.response.PrivacyResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrivacyService {
+
+    public PrivacyResponse getPrivacyPolicy() {
+
+
+    }
+}
