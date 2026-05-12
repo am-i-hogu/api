f42357efc8f5cd2711378e712d224ecb809efb76
mj0107 <alswns1285@gmail.com>
2026-05-12 17:45:22 +0900

    feat: health-check 구현

---
+++ src/main/java/com/hogu/am_i_hogu/common/health/HealthCheckController.java
@@ -0,0 +1,40 @@
+package com.hogu.am_i_hogu.common.health;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    private final String env;
+    private final int serverPort;
+    private final String serverAddress;
+    private final String serverName;
+
+    public HealthCheckController(
+            @Value("${server.env}") String env,
+            @Value("${server.port}") int serverPort,
+            @Value("${server.serverAddress}") String serverAddress,
+            @Value("${serverName}") String serverName
+    ) {
+        this.env = env;
+        this.serverPort = serverPort;
+        this.serverAddress = serverAddress;
+        this.serverName = serverName;
+    }
+
+    @GetMapping("/api/health")
+    public HealthCheckResponse healthCheck() {
+        return new HealthCheckResponse("UP", env, serverPort, serverAddress, serverName);
+    }
+
+    public record HealthCheckResponse(
+            String status,
+            String env,
+            int serverPort,
+            String serverAddress,
+            String serverName
+    ) {
+    }
+}
