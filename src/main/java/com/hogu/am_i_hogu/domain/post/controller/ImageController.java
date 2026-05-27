7fdd2d61d968b7448b4c1b4365610eb3fae7e323
mj0107 <alswns1285@gmail.com>
2026-05-27 23:52:10 +0900

    feat: 인증 정보 요구하도록 구현

--- src/main/java/com/hogu/am_i_hogu/domain/post/controller/ImageController.java
+++ src/main/java/com/hogu/am_i_hogu/domain/post/controller/ImageController.java
@@ -5,6 +5,7 @@ import com.hogu.am_i_hogu.domain.post.service.ImageUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -20,9 +21,11 @@ public class ImageController implements ImageApiDoc {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ImageUploadResponse> uploadImage(
+            Authentication authentication,
             @RequestPart(value = "image", required = false) MultipartFile image
     ) {
-        String imageUrl = imageUploadService.upload(image);
+        Long userId = Long.valueOf(authentication.getName());
+        String imageUrl = imageUploadService.upload(userId, image);
 
         return ResponseEntity.ok(new ImageUploadResponse(imageUrl));
     }
