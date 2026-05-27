d050cbd12dd05b66f4e875ed79fa33e1c8399e94
mj0107 <alswns1285@gmail.com>
2026-05-27 20:36:04 +0900

    feat: 게시물 생성시 같은 이미지에 대한 Lock 적용

--- src/main/java/com/hogu/am_i_hogu/domain/post/repository/ImageAssetRepository.java
+++ src/main/java/com/hogu/am_i_hogu/domain/post/repository/ImageAssetRepository.java
@@ -1,7 +1,9 @@
 package com.hogu.am_i_hogu.domain.post.repository;
 
 import com.hogu.am_i_hogu.domain.post.domain.ImageAsset;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,7 @@ import java.util.List;
 @Repository
 public interface ImageAssetRepository extends JpaRepository<ImageAsset, Long> {
     List<ImageAsset> findByPost_IdOrderBySortOrderAsc(Long postId);
-    void deleteByPost_Id(Long postId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<ImageAsset> findAllWithLockByUrlInOrderByUrlAsc(List<String> urls);
 }
